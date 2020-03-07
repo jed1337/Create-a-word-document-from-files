09615962396480a9a2146ac1c46cb3e5f31291bb
jed1337 <j.caychingco@yahoo.com>
2020-03-08 01:08:14 +0800

    Output data as a word document

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from path import Path
 from PathHolder import PathHolder
+from docx import Document
 
 
 def print_all_files(path):
@@ -11,9 +12,28 @@ def print_all_files(path):
         print(f"{path} is a file")
 
 
+def parent_and_basename(path):
+    return path.parent.name+"\\"+path.name
+
+
+def save_to_word(path_holders):
+    document = Document()
+    document.add_heading('Document Title', level=0)
+
+    document.add_heading('Scripts', level=1)
+
+    for path_holder in path_holders:
+        document.add_paragraph(path_holder.path.name, style='List Number')
+        for script_name in path_holder.script_names:
+            document.add_paragraph(parent_and_basename(script_name), style='List Bullet 2')
+
+    document.add_heading('VERIFICATION', level=1)
+    for path_holder in path_holders:
+        document.add_paragraph(path_holder.path.name, style='List Number')
+        for verification_script_name in path_holder.verification_script_names:
+            document.add_paragraph(parent_and_basename(verification_script_name), style='List Bullet 2')
+
+    document.save('test.docx')
+
 path = Path(r"D:\path")
-print_all_files(path)
-
 path_holders = [PathHolder(sub_path) for sub_path in path.listdir()]
-
-print(path_holders)
