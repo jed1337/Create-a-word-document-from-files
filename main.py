7e394cde4a1fd0b6d6c6cc84b555241e73876548
jed1337 <j.caychingco@yahoo.com>
2020-03-07 13:24:47 +0800

    Added get sub directories method

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from path import Path
 
 
-def print_all_files(input):
-    if(input.isdir()):
-        for sub_directory in input.listdir():
-            print(f"{sub_directory} is a sub directory")
-            print_all_files(sub_directory)
-    elif(input.isfile()):
-        print(f"{input} is a file")
+def print_all_files(path):
+    if(path.isdir()):
+        print(f"{path} is a directory")
+        for sub_path in path.listdir():
+            print_all_files(sub_path)
+    elif(path.isfile()):
+        print(f"{path} is a file")
 
+def get_sub_directories(path):
+    return [sub_path.name for sub_path in path.listdir()]
 
 root_directory = Path(r"D:\path")
 print_all_files(root_directory)
